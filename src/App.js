14f13fd1748ac37b3d62a1b8ded300f1f215d0eb
vikira <y29@naver.com>
2021-09-22 16:46:32 +0900

    front init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Form } from 'react-bootstrap';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { map } from 'lodash';
 
 function App() {
+  const [input, setInput] = useState('');
+  const [todos, setTodos] = useState([]);
+
+  const getTodos = async () => {
+    const result = await axios.get('http://localhost:5000/todos').catch((e) => {
+      console.log(e);
+    });
+    if (result && result.status === 200) {
+      setTodos(result.data);
+    }
+  };
+
+  useEffect(() => {
+    getTodos();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className='App'>
+      <h1>Todo list</h1>
+
+      <Form>
+        <Form.Group className='mb-3' controlId='formBasicEmail'>
+          <Form.Label>Todo를 입력하는 곳</Form.Label>
+          <Form.Control
+            type='text'
+            placeholder='todo를 입력하세요'
+            onChange={(e) => setInput(e.target.value)}
+            value={input}
+          />
+        </Form.Group>
+
+        <Button
+          variant='primary'
+          type='submit'
+          onClick={() => alert('clicked')}
         >
-          Learn React
-        </a>
-      </header>
+          Submit
+        </Button>
+      </Form>
+
+      {todos &&
+        map(todos, (item) => (
+          <Button onClick={() => alert(`${item?.id} clicked`)}>
+            {item?.title}
+          </Button>
+        ))}
     </div>
   );
 }
